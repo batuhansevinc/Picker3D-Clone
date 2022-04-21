838c510cff0cd969aa77aa435879704d9ef13411
batuhansevinc <61507580+batuhansevinc@users.noreply.github.com>
2022-04-22 01:11:54 +0300

    FinalVersion

---
+++ Assets/ObjectExplosion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Picker3d
+{
+    public class ObjectExplosion : MonoBehaviour
+    {
+        [SerializeField] private GameObject miniobject;
+
+        private void OnTriggerEnter(Collider other)
+        {
+
+            if (other.tag == "Player")
+            {
+                CreateMiniObjects();
+            }
+        }
+
+        public void CreateMiniObjects()
+        {
+            GameObject miniobjects = Instantiate(miniobject, transform.position + Vector3.down, Quaternion.identity);
+            miniobjects.transform.parent = transform.parent;
+            transform.GetComponent<MeshRenderer>().enabled = false;
+            foreach (Transform child in miniobjects.transform)
+            {
+                if (child.transform.GetComponent<Rigidbody>() != null)
+                {
+                    child.transform.GetComponent<Rigidbody>().AddForce(Vector3.back * 10f);
+
+                }
+            }
+        }
+    }
+}
